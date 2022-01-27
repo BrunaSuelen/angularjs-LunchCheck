8d81a08d91b051932a465670ae00cf929e9254f4
C1324737 Bruna Suelen Garcia Rosa Silva <c1324737@interno.bb.com.br>
2022-01-27 15:45:27 -0300

    feat: Adiciona funcionalidade

---
+++ app.js
@@ -0,0 +1,35 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('LunchCheck', [])
+    .controller('LunchCheckController', LunchCheckController);
+
+  LunchCheckController.$inject = ['$scope'];
+
+  function LunchCheckController($scope) {
+    $scope.lunch = '';
+    $scope.message = '';
+
+    $scope.checkIfTooMuch = function() {
+      const LUNCH_ITEMS = $scope.separateLunchItems();
+
+      if (!LUNCH_ITEMS.length) {
+        return $scope.message = 'Please enter data first';
+      }
+
+      $scope.message = LUNCH_ITEMS.length <= 3 
+        ? 'Enjoy!'
+        : 'Too much!';
+    }
+
+    $scope.separateLunchItems = function() {
+      return $scope.lunch
+        .split(',')
+        .filter(function(item) {
+          return item != '' && item != ' ';
+        });
+    }
+  }
+}
+)()
